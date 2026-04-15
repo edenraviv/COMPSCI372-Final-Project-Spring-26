cd880420a18872257c8531952460ade56afa3783
Sabina Eraso <sabina.eraso@duke.edu>
2026-04-15 10:33:51 -0400

    feat: basic send_get method

---
+++ src/ingress.py
@@ -0,0 +1,8 @@
+import requests
+
+def send_get_request(self, url, api_key):
+    params = {'api_dev_key':api_key,
+            'api_option':'paste',
+            'api_paste_format':'python'}
+    response = requests.get(url, params)
+    return response.json()
---
+++ utils/evaluation.py
@@ -0,0 +1,1 @@
+#Calibration metrics, Brier score, backtest 
