a919001674cc5bce0b1a9c22562bfe6f8da2910f
sabinaeraso <sabina.eraso05@gmail.com>
2026-04-19 16:58:53 -0400

    remove broad keywords

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -23,10 +23,10 @@ def is_valid_market(m: dict, question) -> bool:
     if m.get("result") not in ["yes", "no"]:
         return False
     
-    keywords = ["election", "US Elections", "Primaries", "House","International elections","Senate","Governor","2028","Peru","Hungary",'Trump', 'Congress',
-      'International', 'SCOTUS & courts', 'Recurring', 'Local', 'Iran', 'Hormuz', 'Strait', 'government', 'Kash Patel', 'Israel',
-      "Attorney", "Cabinet", "Venezuela", "Hegseth", "Americans", "tariffs", "DHS", "ICE", "citizenship", "voter", "legislation", 
-      "Justice", "Supreme Court", "Senators", "Fed chair", "federal crime", "approval rating", "defense funding", "boycott", "executive order", "law", "Powell", "Commissioner", "Cory Mills"
+    keywords = ["election", "US Elections", "Primaries", "House","International elections","Senate","Governor",'Trump', 'Congress',
+      'International', 'SCOTUS & courts', 'Recurring', 'Iran', 'Hormuz', 'Strait', 'government', 'Kash Patel',
+      "Attorney", "Cabinet", "Venezuela", "Hegseth", "Americans", "tariffs", "DHS", "citizenship", "voter", "legislation", 
+      "Justice", "Supreme Court", "Senators", "Fed chair", "federal crime", "approval rating", "defense funding", "boycott", "executive order", "Powell", "Commissioner", "Cory Mills"
       , "pardon", "embassy", "Truth Social", "Secretary of Labor", "Presidency", "Pam Bondi", "Mamdani", "Kamala Harris", "Representative"]
     
     def contains_keyword(question, keywords):
