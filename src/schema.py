b87d104a145437d22297f143b2b8b48cc33e57c9
sabinaeraso <sabina.eraso05@gmail.com>
2026-04-23 17:07:11 -0400

    remove unused imports in schema

--- src/schema.py
+++ src/schema.py
@@ -1,8 +1,6 @@
 
 from dataclasses import dataclass
 from typing import Optional
-from torch.utils.data import Dataset
-import torch
 
 
 # ---------------------------------------------------------------------------
